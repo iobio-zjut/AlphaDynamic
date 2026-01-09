9c9622e510d4ebe80d1921fc7bc9417543f54283
iobio-zjut <43274315+iobio-zjut@users.noreply.github.com>
2026-01-09 15:53:32 +0800

    Modify BASE_DIR and related environment variables
    Updated directory paths and added new environment variables.

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,11 +1,14 @@
 # config.sh
 
-export BASE_DIR="/AlphaFlex"
+export BASE_DIR="/xsdata/gelingyu/AlphaFlex/webserver/AlphaDynamic_github"
 export pdb_dir="$BASE_DIR/input/pdb"
 export fasta_dir="$BASE_DIR/input/sequence"
+export filter_list="$BASE_DIR/input/list.txt"
 export msa_out_dir="$BASE_DIR/predict"
 export flagfile="$BASE_DIR/af_multiple_conformation/monomer_full_dbs.flag"
-export target_db="/Alphafold_data/AFDB/AFDB_db"
-export filter_list="/AlphaFlex/input/list.txt"
-export template_dir="/Alphafold_data/AFDB"
+export target_db="/node1data/gelingyu/foldseek/PDB_AFDB_207187/PDB_AFDB_db"
+export template_dir="/home/data/database/AFDB/PDB_AFDB_207187"
+export msa_database_dir="/home/data/database/Colabfold_MSA"
 export num_threads=1
+export num_comformations=1000
+
